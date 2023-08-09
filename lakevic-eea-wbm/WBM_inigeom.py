69eb4bf67d4231055ab9adec80ceea2250a0216a
rosapietroiusti <72025992+rosapietroiusti@users.noreply.github.com>
2023-08-09 15:23:00 +0200

    Update WBM_inigeom.py

--- lakevic-eea-wbm/WBM_inigeom.py
+++ lakevic-eea-wbm/WBM_inigeom.py
@@ -22,20 +22,10 @@ sys.path.append(WBM_path)
 import WBM_inicon as inicon 
 import WBM_settings as settings 
 
-
-# initialise Lake Victoria boundaries - do I need this ? 
-# lat_min_Vict = -3.1
-# lat_max_Vict = 0.6
-# lon_min_Vict = 31.4
-# lon_max_Vict = 35
-# bounds_Vict  = [lat_min_Vict, lat_max_Vict, lon_min_Vict, lon_max_Vict]
-
-
 # define size of a grid point (grid resolution)
 res_grid = 0.065 # pixel length [°]
 res_m = res_grid*inicon.c_earth/360 # pixel length [m] - at the equator?
 A_cell = res_m**2 # pixel area - note, using this is inconsistent with A_lake being calculated by projecting shapefile and then calculating area
-
 
 # Calculate area of lake - projecting shapefiles (compare these values with literature)
 lake_shp = gpd.read_file(settings.filepath_shp_lake, crs="epsg:4326") # check this is correct crs, check what file this is in
@@ -45,23 +35,14 @@ basin_proj = basin_shp.to_crs("EPSG:32736")
 A_lake = sum(lake_proj.area) # m^2
 A_basin = sum(basin_proj.area) # m^2
 
-
 # Alternative A_lake values 
 A_lake_old = 68272645811.8022 # m^2 # (Vanderkelen 2018) was calculated as lake pixel number x A_cell # note pixel number of lake mask are slightly different in python version here cfr to origianl MATLAB version
 A_lake_shp = lake_shp['Lake_area'].iloc[0] * 10**6 # from km2 to m2 # shapefile metadata
 
-
-# Get grid coordinates from Ppn file --> might not need this if masks are made in main script
-# var = 'precipitation'
-# with xr.open_dataset(settings.filepath_precip, decode_coords="all") as ds:
-#     lats_precip = np.array(ds.get('lat'))
-#     lons_precip = np.array(ds.get('lon'))
-
+# Get grid coordinates from Ppn file --> do not need this if masks are made in main script
 
 # Make lake and basin masks  --> I moved this to main script, it was making problems here (small difs in lat/lon probably)
-#rmask_lake = regionmask.mask_geopandas(lake_shp, lons_precip, lats_precip) + 1       # only lake set = 1
-#rmask_basinlake = regionmask.mask_geopandas(basin_shp, lons_precip, lats_precip) + 1 # lake+ basin set = 1
-#rmask_basin = rmask_basinlake * np.where(np.isnan(rmask_lake),1,np.nan) # only basin set = 1
+
 
 
 
